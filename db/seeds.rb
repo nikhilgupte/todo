685584b5cfa4755e1683dd00ee812125af74a6a7
Nikhil Gupte <nikhilgupte@gmail.com>
2019-09-16 09:06:35 -0400

    Added seed data based on Portman tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Tag.find_or_create_by(title: 'Today')
+wash_laundary = Task.find_or_create_by(title: 'Wash laundry')
+wash_laundary.tag_names = ['Today']
